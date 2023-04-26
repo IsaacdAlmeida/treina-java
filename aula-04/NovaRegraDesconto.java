c06b5e53c0203e401123f8ef399a972c00c564aa
Isaac Almeida <isaacalmeidafilho@gmail.com>
2023-04-25 21:08:01 -0300

    complemento aula 05

--- aula-04/NovaRegraDesconto.java
+++ aula-04/NovaRegraDesconto.java
@@ -3,7 +3,9 @@ public class NovaRegraDesconto {
     Autor livroEbook = new Autor();
     livroEbook.setAuthorName("Joaquim");
 
-    // Ebook ebook = new Ebook(livroEbook);
+    Ebook ebook = new Ebook(livroEbook);
+
+    ebook.setBookPrice(80);
 
     // System.out.println(ebook.getAuthor().getAuthorName());
 
@@ -11,6 +13,14 @@ public class NovaRegraDesconto {
 
     novoLivro.setBookPrice(100);
 
-    System.out.println(novoLivro.getTaxaImpressao());
+    // System.out.println(novoLivro.getTaxaImpressao());
+
+    CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+
+    carrinho.adicionaAoCarrinho(novoLivro);
+    carrinho.adicionaAoCarrinho(ebook);
+
+    System.out.println(carrinho.getTotal());
+
   }
 }
